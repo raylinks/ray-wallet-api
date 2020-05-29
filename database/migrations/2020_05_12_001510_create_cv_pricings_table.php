82811f1d0725f2dc4358c650269a8e02e80a4159
rilwan <olawuyirilwan98@gmail.com>
2020-05-29 23:29:47 +0100

    spartie

--- database/migrations/2020_05_12_001510_create_cv_pricings_table.php
+++ database/migrations/2020_05_12_001510_create_cv_pricings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Createcv_pricingsTable extends Migration
+class CreateCvPricingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,6 +29,6 @@ class Createcv_pricingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cv_pricings');
+        Schema::dropIfExists('cvpricings');
     }
 }
